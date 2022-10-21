3cf766fb7de66b7438d8e12b6dbd71a3f743b382
Henrik Alsér <henrik.alser@me.com>
2022-10-21 15:13:09 +0200

    Add channel example

--- src/bin/channel.rs
+++ src/bin/channel.rs
@@ -7,6 +7,8 @@ use embassy_executor::Executor;
 use embassy_executor::_export::StaticCell;
 use embassy_rp::gpio::{Level, Output};
 use embassy_rp::peripherals::PIN_25;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::channel::Channel;
 use embassy_time::{Duration, Timer};
 use hal::multicore::{Multicore, Stack};
 use hal::pac;
@@ -14,9 +16,14 @@ use rp2040_hal as hal;
 use {defmt_rtt as _, panic_probe as _};
 
 static mut CORE1_STACK: Stack<4096> = Stack::new();
-
 static EXECUTOR0: StaticCell<Executor> = StaticCell::new();
 static EXECUTOR1: StaticCell<Executor> = StaticCell::new();
+static CHANNEL: Channel<CriticalSectionRawMutex, LedState, 1> = Channel::new();
+
+enum LedState {
+    On,
+    Off,
+}
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -39,16 +46,22 @@ fn main() -> ! {
 
 #[embassy_executor::task]
 async fn core0_task() {
+    info!("Hello from core 0");
     loop {
-        info!("Hello from CORE0");
-        Timer::after(Duration::from_secs(1)).await;
+        CHANNEL.send(LedState::On).await;
+        Timer::after(Duration::from_millis(100)).await;
+        CHANNEL.send(LedState::Off).await;
+        Timer::after(Duration::from_millis(400)).await;
     }
 }
 
 #[embassy_executor::task]
 async fn core1_task(mut led: Output<'static, PIN_25>) {
+    info!("Hello from core 1");
     loop {
-        led.toggle();
-        Timer::after(Duration::from_millis(500)).await;
+        match CHANNEL.recv().await {
+            LedState::On => led.set_high(),
+            LedState::Off => led.set_low(),
+        }
     }
 }
